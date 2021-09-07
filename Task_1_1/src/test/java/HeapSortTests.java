c9a59f867bf286f017402a495759a4fb38690c63
Alexander Dolgiy <alex20021505@gmail.com>
2021-09-07 18:13:19 +0700

    Updated JUnit version, added paramerized tests

--- Task_1_1/src/test/java/HeapSortTests.java
+++ Task_1_1/src/test/java/HeapSortTests.java
@@ -1,38 +1,44 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("Heap sort test")
 public class HeapSortTests {
-    @Test
-    public void simpleTests() {
-        // first (example) test
-        int[] arr = {5, 4, 3, 2, 1};
-        int[] ans = {1, 2, 3, 4, 5};
-        HeapSort.heapSort(arr, arr.length);
-        assertArrayEquals(ans, arr);
 
-        // second test
-        int[] arr2 = {-3, 1024, 753, 22, 1};
-        int[] ans2 = {-3, 1, 22, 753, 1024};
-        HeapSort.heapSort(arr2, arr2.length);
-        assertArrayEquals(ans2, arr2);
+    private Stream<Arguments> getParametersForTest() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{5, 4, 3, 2, 1},
+                        new int[]{1, 2, 3, 4, 5}
+                ),
+                Arguments.of(
+                        new int[]{-3, 1024, 753, 22, 1},
+                        new int[]{-3, 1, 22, 753, 1024}
+                ),
+                Arguments.of(
+                        new int[0],
+                        new int[0]
+                ),
+                Arguments.of(
+                        new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, -1, 1},
+                        new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE, Integer.MAX_VALUE}
+                )
+        );
     }
 
-    @Test
-    public void emptyArrayTest() {
-        int[] arr = {};
-        int[] ans = {};
+    @DisplayName("Heap sort test")
+    @ParameterizedTest
+    @MethodSource("getParametersForTest")
+    public void emptyArrayTest(int[] arr, int[] ans) {
         HeapSort.heapSort(arr, arr.length);
         assertArrayEquals(ans, arr);
-    }
-
-    @Test
-    public void maxIntNumbersTest() {
-        int[] arr = {Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, -1, 1};
-        int[] ans = {Integer.MIN_VALUE, Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE, Integer.MAX_VALUE};
-        HeapSort.heapSort(arr, arr.length);
-        assertArrayEquals(ans, ans);
     }
 }
