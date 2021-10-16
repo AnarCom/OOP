fa17a8a0efba105badc78d94ece360fb7c81dc25
Alexander Dolgiy <alex20021505@gmail.com>
2021-10-16 17:12:12 +0700

    added semester test

--- Task_3_2/src/main/java/Pair.java
+++ Task_3_2/src/main/java/Pair.java
@@ -1,9 +1,11 @@
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * Class for pair. T is type of first argument, L - second.
  */
 @Data
+@AllArgsConstructor
 public class Pair<T, L> {
     private T first;
     private L second;
