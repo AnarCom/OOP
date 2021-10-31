70f916c43040fbfae73ed2c6e529142ce892a5ed
Alexander Dolgiy <alex20021505@gmail.com>
2021-10-31 13:22:27 +0700

    added test for main class

--- Task_4_1/src/main/java/Main.java
+++ Task_4_1/src/main/java/Main.java
@@ -2,11 +2,14 @@ import calculator.MyCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length != 1){
+        System.out.println(calculate(args));
+    }
+
+    public static Double calculate(String[] args) {
+        if (args.length != 1) {
             throw new IllegalArgumentException("You have to pass only 1 string");
         }
-
         MyCalculator calculator = new MyCalculator();
-        System.out.println(args[0]);
+        return calculator.calc(args[0]);
     }
 }
---
+++ Task_4_1/src/test/java/MainTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest {
+
+    public static Stream<Arguments> calculateTestSource() {
+        return Stream.of(
+                Arguments.of(3D, "+ 1 2"),
+                Arguments.of(0D, "sin + - 1 2 1"),
+                Arguments.of(3D, "+ cos 0 2"),
+                Arguments.of(1024D, "pow 2 10"),
+                Arguments.of(0D, "ln 1"),
+                Arguments.of(15D, "* + 1 2 5")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("calculateTestSource")
+    public void calculateTest(Double ans, String formula) {
+        Assertions.assertEquals(
+                ans,
+                Main.calculate(new String[]{formula})
+        );
+    }
+
+    @Test
+    public void tooManyArgumentsTest() {
+        var a = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> Main.calculate(new String[]{"", ""})
+        );
+        Assertions.assertEquals("You have to pass only 1 string", a.getMessage());
+    }
+
+    @Test
+    public void tooFewArgumentsTest() {
+        var a = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> Main.calculate(new String[0])
+        );
+        Assertions.assertEquals("You have to pass only 1 string", a.getMessage());
+    }
+}
